069b681281637fcbe548a873ba3dc7e4d6186519
abhisheksingla <abhishek.singhla@sourcefuse.com>
2016-05-06 04:48:41 +0530

    new files added

---
+++ SportsPal/ViewControllers/ViewController/TB_AddTeam.h
@@ -0,0 +1,18 @@
+//
+//  TB_Play_Teams.h
+//  SportsPal
+//
+//  Created by Abhishek Singla on 19/04/16.
+//  Copyright © 2016 SportsPal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TB_AddTeam: UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgProfile;
+@property (weak, nonatomic) IBOutlet UIImageView *imgAdmin;
+@property (weak, nonatomic) IBOutlet UILabel *lblName;
+
+
+@end
